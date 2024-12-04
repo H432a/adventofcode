2e5c3eee2d2d52d3c9124ec2ce91dea5867048c6
Hamritha V <hamrithav3@gmail.com>
2024-12-04 23:27:21 +0530

    Add files via upload

---
+++ day01/advent_day1_p1.py
@@ -0,0 +1,27 @@
+with open('input.txt', 'r') as file:
+    data = file.readlines()
+
+list1 = []
+list2 = []
+
+for line in data:
+    num1, num2 = map(int, line.split())
+    list1.append(num1)
+    list2.append(num2)
+
+print("List 1:", list1)
+print("List 2:", list2)
+
+sum = 0
+
+while list1 and list2:
+    min1 = min(list1)
+    min2 = min(list2)
+
+    diff = abs(min1 - min2)
+    sum += diff
+
+    list1.remove(min1)
+    list2.remove(min2)
+
+print(sum)
